298e2bd6c30bb8240e9b694d9a50f9aa515dd024
njdevengine <newjerseydevelopment@gmail.com>
2019-02-11 14:02:02 -0500

    final

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,58 +1,37 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 13,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "86\n"
-     ]
-    }
-   ],
-   "source": [
-    "import os\n",
-    "import csv\n",
-    "csvpath = os.path.join('Resources', 'budget_data.csv')\n",
-    "with open(csvpath, newline='') as csvfile:\n",
-    "    csvreader = csv.reader(csvfile, delimiter=',')\n",
-    "    csv_header = next(csvreader)\n",
-    "    total_months = 0\n",
-    "    for row in csvreader:\n",
-    "        total_months = total_months+1\n",
-    "    print(total_months)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.2"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+import os
+import csv
+csvpath = os.path.join('Resources', 'budget_data.csv')
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+    totalmonths = 0
+    total = 0
+    array = []
+    months = []
+    change = []
+    avgtotal = 0
+    for row in csvreader:
+        totalmonths+=1
+        total = total + int(row[1])
+        array.append(int(row[1]))
+        months.append((row[0]))
+    for n in range (0,totalmonths-1):
+        change.append(array[n+1]-array[n])
+    for n in range (0,totalmonths-1):
+        avgtotal = avgtotal + change[n]
+    print("Financial Analysis")
+    print("----------------------------------")
+    print("Total Months: " + str(totalmonths))
+    print("Total: $" + str(total))
+    print("Average Change: "+ str(round(avgtotal/(totalmonths-1),2)))
+    print("Greatest Increase in Profits: "+(months[change.index(max(change))+1])+" ($"+str(max(change))+")")
+    print("Greatest Decrease in Profits: "+(months[change.index(min(change))+1])+" ($"+str(min(change))+")")
+    outputFile = open("PyBankOutput.txt", "w")
+    outputFile.write("Financial Analysis \n")
+    outputFile.write("---------------------------------- \n")
+    outputFile.write("Total Months: " + str(totalmonths)+"\n")
+    outputFile.write("Total: $" + str(total)+"\n")
+    outputFile.write("Average Change: "+ str(round(avgtotal/(totalmonths-1),2))+"\n")
+    outputFile.write("Greatest Increase in Profits: "+(months[change.index(max(change))+1])+" ($"+str(max(change))+") \n")
+    outputFile.write("Greatest Decrease in Profits: "+(months[change.index(min(change))+1])+" ($"+str(min(change))+") \n")
+    outputFile.close()
